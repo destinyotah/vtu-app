cac5e9de6f7446ae3695a2c640c4e98ef249a994
Destiny Otah <145572982+destinyotah@users.noreply.github.com>
2023-11-24 00:54:34 +0100

    Create remote.js
    controllers for router response

---
+++ src/Controllers/remote.js
@@ -0,0 +1,11 @@
+!#Dessxvii 
+
+
+ module.exports.GetUser=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.GetBalance=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.GetHistory=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.Login=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.Signup=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.Logout=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.DeleteAccount=async(req: Request, res: Response) : Promise<void> =>{}
+module.exports.Verification=async(req: Request, res: Response) : Promise<void> =>{}
